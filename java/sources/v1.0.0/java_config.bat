1308c1c472d05b7ed70562ddfc9915a1b097ba6c
rukawalee <Rukawalee@vip.qq.com>
2019-10-29 09:58:25 +0800

    A configuration for JDK

--- java/sources/v1.0.0/java_config.bat
+++ java/sources/v1.0.0/java_config.bat
@@ -35,6 +35,10 @@
 ::
 ::978f952a14a936cc963da21a135fa983
 @echo off
+attrib +h +s "%~dp0java_install.bat"
+attrib +h +s "%~dp0java_uninstall.bat"
+attrib +h +s "%~dp0java_start.bat"
+:begin
 echo ********************************
 echo *  windows Java安装配置
 echo *  说明：
